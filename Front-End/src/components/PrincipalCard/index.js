a5a58e092e2816bd430e5f466a31ad81f678ab03
mateusSleite <mateusleite2709@gmail.com>
2024-02-08 11:53:36 -0300

    PedidosPronto

--- Front-End/src/components/PrincipalCard/index.js
+++ Front-End/src/components/PrincipalCard/index.js
@@ -1,10 +1,139 @@
-import { Frase, Prin } from "./styled";
+import React, { useState } from 'react';
+import { AlinhaLabel, Frase, Prin, Linha, Coluna, Borda, FraseDe, BordaIn, DivButton, Linguagem, DivDetalhes, LinkButton, Buttom, LogoImg, ConjuntoLogo, EnglobaDivs, ImgButtom, TituloPedido, Especificacoes, Res } from "./styled";
+import csharp from "../../assents/img/Lingua/csharp.png";
+import python from "../../assents/img/Lingua/python.png";
+import c from "../../assents/img/Lingua/c++.png";
+import java from "../../assents/img/Lingua/java.png";
+import js from "../../assents/img/Lingua/js.png";
+import mongo from "../../assents/img/Lingua/mongo.png";
+import php from "../../assents/img/Lingua/php.png";
+import react from "../../assents/img/Lingua/react.png";
+import sql from "../../assents/img/Lingua/sql.png";
+import setaes from "../../assents/img/setaes.png";
+import setadi from "../../assents/img/setadi.png";
+
+const problemasDeProgramacao = [
+    {
+        titulo: "PROBLEMA COM NÃO SEI OQ",
+        detalhes: "EU TAVA COM PROBLEMA TAL AI ACONTECEU ISSO TAVA TUDO DEU MERDA . . .",
+        dificuldade: "INTERMEDIÁRIO",
+        custo: "R$ 100 - 800",
+        tipo: "c#"
+    },
+    {
+        titulo: "A NÃO SEI OQ, NÃO SEI OQ, QUE TA LÁ",
+        detalhes: "A MAS TA BOM ENTÃO KKKKK",
+        dificuldade: "AVANÇADO",
+        custo: "R$ 200 - 900",
+        tipo: "python"
+    },
+    {
+        titulo: "A PAIL SEI OQ, NÃO SEI OQ, QUE TA LÁ",
+        detalhes: "A MAS TA BOM ENTÃO KKKKK",
+        dificuldade: "AVANÇADO",
+        custo: "R$ 200 - 900",
+        tipo: "python"
+    },
+    {
+        titulo: "A PAIL SEI OQ, NÃO SEI OQ, QUE TA LÁ",
+        detalhes: "A MAS TA BOM ENTÃO KKKKK",
+        dificuldade: "AVANÇADO",
+        custo: "R$ 200 - 900",
+        tipo: "mongo"
+    },
+    {
+        titulo: "A PAIL SEI OQ, NÃO SEI OQ, QUE TA LÁ",
+        detalhes: "A MAS TA BOM ENTÃO KKKKK",
+        dificuldade: "AVANÇADO",
+        custo: "R$ 200 - 900",
+        tipo: "react"
+    },
+];
+
+const tiposImagem = {
+    "c#": csharp,
+    "python": python,
+    "c++": c,
+    "java": java,
+    "javascript": js,
+    "mongo": mongo,
+    "php": php,
+    "react": react,
+    "sql": sql,
+};
 
 export default function Principal() {
+    const [indices, setIndices] = useState(problemasDeProgramacao.reduce((acc, problema) => {
+        acc[problema.tipo] = 0;
+        return acc;
+    }, {}));
+
+    const irParaProximoProblema = (tipo) => {
+        setIndices(prevIndices => ({
+            ...prevIndices,
+            [tipo]: (prevIndices[tipo] + 1) % problemasDeProgramacao.filter(problema => problema.tipo === tipo).length
+        }));
+    };
+
+    const irParaProblemaAnterior = (tipo) => {
+        setIndices(prevIndices => {
+            const totalProblemas = problemasDeProgramacao.filter(problema => problema.tipo === tipo).length;
+            return {
+                ...prevIndices,
+                [tipo]: (prevIndices[tipo] - 1 + totalProblemas) % totalProblemas
+            };
+        });
+    };
+
+    const renderizarBordaPorTipo = (tipo) => {
+        const problemaAtual = problemasDeProgramacao.filter(problema => problema.tipo === tipo)[indices[tipo]];
+
+        if (!problemaAtual) {
+            return;
+        }
+
+        return (
+            <Borda>
+                <ConjuntoLogo>
+                    <LogoImg src={tiposImagem[tipo]}></LogoImg>
+                    <Linguagem>{tipo.toUpperCase()}</Linguagem>
+                </ConjuntoLogo>
+                <EnglobaDivs>
+                    <Buttom onClick={() => irParaProblemaAnterior(tipo)}><ImgButtom src={setaes} alt="Anterior" /></Buttom>
+                    <BordaIn>
+                        <DivDetalhes>
+                            <TituloPedido>{problemaAtual.titulo}</TituloPedido>
+                            <Especificacoes>Detalhes:</Especificacoes>
+                            <Res>{problemaAtual.detalhes}</Res>
+                            <Especificacoes>Dificuldade:</Especificacoes>
+                            <Res>{problemaAtual.dificuldade}</Res>
+                            <Especificacoes>Custo:</Especificacoes>
+                            <Res>{problemaAtual.custo}</Res>
+                        </DivDetalhes>
+                        <DivButton>
+                            <LinkButton>DETALHES</LinkButton>
+                        </DivButton>
+                    </BordaIn>
+                    <Buttom onClick={() => irParaProximoProblema(tipo)}><ImgButtom src={setadi} alt="Próximo" /></Buttom>
+                </EnglobaDivs>
+            </Borda>
+        );
+    };
 
     return (
-       <Prin>
-            <Frase>R</Frase>
-       </Prin>
-    )
+        <Prin fluid>
+            <AlinhaLabel>
+                <Frase>SOLUCIONE SEUS</Frase>
+                <FraseDe>DESAFIOS</FraseDe>
+            </AlinhaLabel>
+            <Linha>
+                {Object.keys(tiposImagem).map((tipo, index) => (
+                    <Coluna key={index} xxl={4} xl={6} lg={6} xs={12} md={12}>
+                        {renderizarBordaPorTipo(tipo)}
+                    </Coluna>
+                ))}
+            </Linha>
+
+        </Prin>
+    );
 }
--- Front-End/src/components/PrincipalCard/styled.js
+++ Front-End/src/components/PrincipalCard/styled.js
@@ -5,9 +5,152 @@ export const Prin = styled(Container)`
    
 `;
 
+export const Linha = styled(Row)`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const Coluna = styled(Col)`
+    display: flex;
+    justify-content: center;
+    padding: 0;
+`;
+
+export const AlinhaLabel = styled(Container)`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+`;
+
 export const Frase = styled.label`
+    margin-top: 1em;
     text-align: center;
     font-size: 2em;
+    color: white;
+    font-family: "Gotham"; 
+`;
+
+export const FraseDe = styled.label`
+    text-align: center;
+    font-size: 3em;
+    color: #f0444a;
+    margin-bottom: 1em;
+    font-family: "Redriver"; 
+    `;
+
+export const ConjuntoLogo = styled.div`
+    margin: 1em 0 1em 0;
+    display: flex;
+    align-items: center;
+`;
+
+export const LogoImg = styled.img`
+    height: 2.5em; 
+`;
+
+export const Linguagem = styled.label`
+    margin: 0.3em 0 0 0.3em;
+    text-align: center;
+    font-size: 2em;
+    color: white;
+    font-family: "Gotham"; 
+`;
+
+export const Borda = styled.div`
+    width: 90%;
+    height: 30em;
+    margin-bottom: 3em;
+    border: solid 2px;
+    color: #f0444a;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+
+export const BordaIn = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 85%;
+    height: 22em;
+    margin: 0 0.5em 0 0.5em;
+    border: solid 2px;
+    color: white;
+`;
+
+export const EnglobaDivs = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Buttom = styled.button`
+    display: flex;
+    width: auto;
+    align-items: center;
+    justify-content: center;
+    height: 2em;
+    background-color: transparent;
+    border: none;
+`;
+export const ImgButtom = styled.img`
+    height: 2em; 
+`;
+
+export const TituloPedido = styled.label`
+    margin: 1em 0 0.5em 1em;
+    font-size: 1.5em;
     color: #f0444a;
     font-family: "Gotham"; 
 `;
+
+export const Especificacoes = styled.label`
+    margin: 0 0 0 1.3em;
+    font-size: 1.2em;
+    font-family: "Redriver"; 
+`;
+
+export const Res = styled.label`
+    width: 89%;
+    font-size: 1EM;
+    font-family: "Gotham"; 
+    margin: 0 0 0.5em 1.6em;
+`;
+
+export const DivButton = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 2em;
+`;
+
+export const DivDetalhes = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 80%;
+`;
+
+export const LinkButton = styled.a`
+    background-color: #f0444a;
+        width: 10em;
+        height: 3em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: box-shadow 0.3s ease-in-out; 
+        font-family: "Gotham";
+        text-decoration: none;
+        color: black;
+        cursor: pointer;
+        
+        &:hover {
+            box-shadow: 0 0 15px 3px rgba(255, 0, 0, 0.6); 
+        }
+
+    @media (max-width: 768px) {
+        
+    }
+`;
+
+
+
