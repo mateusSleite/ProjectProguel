37e29a8a9c1de774955d83331bae44307c5ffca7
Renato Mendes <rnt.mendes11@gmail.com>
2024-02-14 10:18:16 -0300

    Navbar

--- Front-End/src/components/NavBar/index.js
+++ Front-End/src/components/NavBar/index.js
@@ -1,39 +1,97 @@
-import React, { useState } from 'react';
-import { Img, LogoGlobal, Nav, Nav2, ImgText, LinkNav, DivNav, DivNav2, LinkButton, LinkButton2, Suporte, MenuButton, DivButton, ImgToggle, Centralize } from "./syled";
-import logo from "../../assents/img/Logo.png"
-import textlogo from "../../assents/img/TextLogo.png"
-import toggle from "../../assents/img/toggle.png"
-import { i18n } from "../../translate/i18n"
-
+import React, { useState } from "react";
+import {
+  Img,
+  LogoGlobal,
+  Nav,
+  Nav2,
+  ImgText,
+  LinkNav,
+  DivNav,
+  DivNav2,
+  LinkButton,
+  LinkButton2,
+  Suporte,
+  MenuButton,
+  DivButton,
+  ImgToggle,
+  Centralize,
+} from "./syled";
+import logo from "../../assents/img/Logo.png";
+import textlogo from "../../assents/img/TextLogo.png";
+import toggle from "../../assents/img/toggle.png";
+import { i18n } from "../../translate/i18n";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 export default function NavBar() {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
 
-    const [isNavVisible, setIsNavVisible] = useState(false);
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+  return (
+    <>
+      <Nav fluid>
+        <LogoGlobal>
+          <Img src={logo}></Img>
+          <ImgText src={textlogo}></ImgText>
+        </LogoGlobal>
+        <DivNav>
+          <LinkNav href="/pedidos">{i18n.t("navbar.requests")}</LinkNav>
+        </DivNav>
+        <DivNav>
+          <LinkNav href="">{i18n.t("navbar.about")}</LinkNav>
+        </DivNav>
+        <DivNav>
+          <LinkNav href="">{i18n.t("navbar.contacts")}</LinkNav>
+        </DivNav>
 
-    return (
-        <>
-            <Nav fluid>
-                <LogoGlobal>
-                    <Img src={logo}></Img>
-                    <ImgText src={textlogo}></ImgText>
-                </LogoGlobal>
-                <DivNav><LinkNav href="/pedidos">{i18n.t("navbar.requests")}</LinkNav></DivNav>
-                <DivNav><LinkNav href="">{i18n.t("navbar.about")}</LinkNav></DivNav>
-                <DivNav><LinkNav href="">{i18n.t("navbar.contacts")}</LinkNav></DivNav>
-                <DivButton>
-                    <LinkButton href="/">{i18n.t("navbar.singup")}</LinkButton>
-                    <MenuButton onClick={() => setIsNavVisible(!isNavVisible)}><ImgToggle src={toggle}></ImgToggle></MenuButton>
-                </DivButton>
-            </Nav>
-            {isNavVisible && <Nav2>
-                <Centralize>
-                    <DivNav2><LinkNav href="">{i18n.t("navbar.requests")}</LinkNav></DivNav2>
-                    <DivNav2><LinkNav href="">{i18n.t("navbar.about")}</LinkNav></DivNav2>
-                    <DivNav2><LinkNav href="">{i18n.t("navbar.contacts")}</LinkNav></DivNav2>
-                    <LinkButton2 href="/">{i18n.t("navbar.singup")}</LinkButton2>
-                </Centralize>
-                <Suporte></Suporte>
-            </Nav2>}
-        </>
-    )
+        <FormControlLabel
+          control={
+            <Switch
+              checked={state.checked}
+              onChange={handleChange}
+              name="checked"
+              style={{
+                color: state.checked ? "#F0444A" : "#feffff",
+              }}
+            />
+          }
+          label="EN"
+          style={{
+            color: "#F0444A",
+          }}
+        />
+
+        <DivButton>
+          <LinkButton href="/">{i18n.t("navbar.singup")}</LinkButton>
+          <MenuButton onClick={() => setIsNavVisible(!isNavVisible)}>
+            <ImgToggle src={toggle}></ImgToggle>
+          </MenuButton>
+        </DivButton>
+      </Nav>
+      {isNavVisible && (
+        <Nav2>
+          <Centralize>
+            <DivNav2>
+              <LinkNav href="">{i18n.t("navbar.requests")}</LinkNav>
+            </DivNav2>
+            <DivNav2>
+              <LinkNav href="">{i18n.t("navbar.about")}</LinkNav>
+            </DivNav2>
+            <DivNav2>
+              <LinkNav href="">{i18n.t("navbar.contacts")}</LinkNav>
+            </DivNav2>
+            <LinkButton2 href="/">{i18n.t("navbar.singup")}</LinkButton2>
+          </Centralize>
+          <Suporte></Suporte>
+        </Nav2>
+      )}
+    </>
+  );
 }
---
+++ Front-End/src/components/NavbarLogin/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Img, LogoGlobal, Nav, Nav2, ImgText, LinkNav, DivNav, DivNav2, LinkButton, LinkButton2, Suporte, MenuButton, DivButton, ImgToggle, Centralize } from "./syled";
+import logo from "../../assents/img/Logo.png"
+import textlogo from "../../assents/img/TextLogo.png"
+import toggle from "../../assents/img/toggle.png"
+import { i18n } from "../../translate/i18n"
+
+
+export default function NavBar() {
+
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    return (
+        <>
+            <Nav fluid>
+                <LogoGlobal>
+                    <Img src={logo}></Img>
+                    <ImgText src={textlogo}></ImgText>
+                </LogoGlobal>
+            </Nav>
+        </>
+    )
+}
